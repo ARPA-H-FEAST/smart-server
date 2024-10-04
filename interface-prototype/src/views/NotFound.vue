ecb0786e92c1191818e9da92d1c1f6861f954552
Pat McNeely <pmcneely@gwu.edu>
2024-10-04 11:29:39 -0400

    Not Found view for router defaults

---
+++ interface-prototype/src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<!-- Vue.Js -->
+<!-- NotFound.vue -->
+<template>
+    <div>
+      <h1>
+        404:
+      </h1>
+      Sorry, this page doesn't exist or has been moved to another location
+    </div>
+</template>
