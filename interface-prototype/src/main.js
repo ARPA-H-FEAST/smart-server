9b593b498cdca9549ee4e56e2126f3c58846eba4
Pat McNeely <pmcneely@gwu.edu>
2024-10-04 11:27:19 -0400

    Add router error tracking - may require it for future debugging

--- interface-prototype/src/main.js
+++ interface-prototype/src/main.js
@@ -9,6 +9,7 @@ import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
+import router from './router'
 
 // Composables
 import { createApp } from 'vue'
@@ -17,7 +18,19 @@ import { createPinia } from 'pinia'
 const pinia = createPinia()
 const app = createApp(App)
 
+router.onError((error, to) => {
+    console.log('---> Router captured error!');
+    if (error.message.includes('error loading dynamically imported module') ||
+        error.message.includes('Failed to fetch dynamically imported module') || 
+        error.message.includes('Importing a module script failed')) {
+            window.location = to.fullPath;
+    } else {
+        console.log('--> Error was %s', error);
+    }
+})
+
 app.use(pinia)
+app.use(router)
 
 registerPlugins(app)
 
