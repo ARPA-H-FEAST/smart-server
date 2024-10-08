2fcd1dbdff92571e78f444b3d986a8371789c631
Pat McNeely <pmcneely@gwu.edu>
2024-10-08 15:31:43 -0400

    Move user information display card into component for re-use

---
+++ interface-prototype/src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+        <v-card>
+            <v-card-title class="text-center">
+                Current User Information
+            </v-card-title>
+            <v-col>
+                User: {{ userStore.user }}<br>
+                Authenticated: {{ userStore.isAuthenticated }}<br>
+                CSRF Token: {{ userStore.token }}<br>
+                OAuth2.0 Code: {{ userStore.code }}<br>
+                OAuth2.0 Token: {{ userStore?.credentials.accessToken }}<br>
+                OAuth2.0 Refresh Token: {{ userStore?.credentials.refreshToken }}
+
+            </v-col>
+        </v-card>
+      </v-container>
+</template>
+<script setup>
+
+import { useUserStore } from '@/stores/user';
+import { onMounted } from 'vue';
+
+const userStore = useUserStore()
+
+
+</script>
