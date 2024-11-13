ab6df4772468ca6742ac2d938114f3d78b94ed9b
Pat McNeely <pmcneely@gwu.edu>
2024-11-13 10:03:56 -0500

    Add Swagger view for possible future use. All references in Vue components *should* have been deprecated and removed

---
+++ interface-prototype/src/views/Swagger.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <v-btn @click.prevent="goToSwagger()">
+        Interact with FHIR
+    </v-btn>
+
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/user';
+
+
+const userStore = useUserStore();
+
+async function goToSwagger() {
+    // window.location.replace("http://localhost:8080/fhir/swagger-ui/")
+    console.log("...Negotiating access to FHIR resources")
+    const response = await fetch("http://localhost:8080/fhir/swagger-ui/", {
+        method: "GET",
+        headers: { "Authorization": "admin" },
+        // headers: { "Authorization": "Bearer " + userStore?.credentials.accessToken },
+        // credentials: 'include',
+    })
+    if (!response.ok) {
+        // Error handling
+        console.log("Caught an error. Le sigh")
+    } else {
+        // Probably will never get here .... More sighs.
+    }
+}
+
+</script>
