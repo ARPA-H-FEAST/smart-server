a8c4fff3fcdf96d79c050fc2e753e932aae9a45e
Pat McNeely <pmcneely@gwu.edu>
2025-02-03 15:25:36 -0500

    WIP - add a mock "dataset-search" endpoint to shim dsviewer endpoints

--- ui/urls.py
+++ ui/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("fhir-metadata/", views.fhir_metadata, name="fhir-metadata"),
     path("fhir-openapi/", views.fhir_openapi, name="fhir-openapi"),
     path("query/", views.query_fhir, name="fhir-queries"),
+    path("dataset/search/", views.check_login_and_get_files, name="dataset-search"),
 ]
