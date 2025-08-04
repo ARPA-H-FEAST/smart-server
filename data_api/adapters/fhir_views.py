6207d8377dbec15122156b2909718ae3acf78897
Pat McNeely <pmcneely@gwu.edu>
2025-08-04 16:55:56 -0400

    WIP - Unclear if separate views will be needed.

---
+++ data_api/adapters/fhir_views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, FileResponse
+
+import json
+import logging
+import os
+
+
+
+def get_data_sources(request):
+
+    ...
