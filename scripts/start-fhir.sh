bd838370a4b932c3e98e620ffe7a8df91c7a669e
Pat McNeely <pmcneely@gwu.edu>
2025-11-06 13:09:05 -0500

    Add FHIR starter. Scrip is not ideal due to issues with the docker image as provided by FHIR folks.

--- scripts/start-fhir.sh
+++ scripts/start-fhir.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# if [ -z $DATA_DIR ]; then
+#     echo This script requires a DATA_DIR to be prvided, aborting
+#     exit 0
+# fi
+
+# docker run -d -p 8080:8080 --network fhir-backplane \
+#     --name fhir-server --volume $DATA_DIR:/app/ feast-hapi-fhir-jpa-starter
+
+
+# NB: Volume mount doesn't work with the gcr container provided by the HAPI FHIR
+# folks. Persistence *does* work as long as the container persists, but this is not
+# ideal
 docker run -d -p 8080:8080 --network fhir-backplane \
-    --name fhir feast-hapi-fhir-jpa-starter
+    --name fhir-server feast-hapi-fhir-jpa-starter
