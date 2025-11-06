48605ee122b0e7436c96c5d895b13a4470392269
Pat McNeely <pmcneely@gwu.edu>
2025-11-06 16:12:03 -0500

    bugfix

--- scripts/populate_fhir_db.py
+++ scripts/populate_fhir_db.py
@@ -277,7 +277,7 @@ if __name__ == "__main__":
                 samples_uploaded = len(patient_data['data'])
                 sample_count -= samples_uploaded
                 offset += samples_uploaded
-                if sample_count > 10000:
+                if offset > 10000:
                     break
                 else:
                     continue
