ce8175300225addc6933ec84d3d05850c27b9f6e
Pat McNeely <pmcneely@gwu.edu>
2025-11-13 08:22:12 -0500

    WIP - Begin breaking out utility logic from larger scripts

---
+++ data_api/db_interfaces/utilities.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+
+def single_parquet_to_df(file_location, file_name, columns=None):
+        # columns = self.config["fhir_columns"][data_type]
+        # print(f"---> Isolating to columns\n{columns}")
+        try:
+            file_path = os.path.join(file_location, file_name)
+            if columns is None:
+                 return pd.read_parquet(file_path)
+            return pd.read_parquet(file_path, columns=columns)
+        except Exception as e:
+            try:
+                file_name = ".BrPrLu.".join(file_name.split("."))
+                print(f"Testing for file {file_name}")
+                if columns is None:
+                    return pd.read_parquet(os.path.join(file_location, file_name))
+                return pd.read_parquet(os.path.join(file_location, file_name), columns=columns)
+            except:
+                raise
+
+def single_table_query_to_string(
+        base_query, query_args, select_func, columns,
+        table_alias, limit=None, offset=None
+    ):
+    query = base_query
+    cols = ",".join(columns)
+    if query_args is not None:
+       query += select_func(query_args)
+
+    if limit is None:
+        query += ";"
+    else:
+        query += f" LIMIT {limit} OFFSET {offset};"
+    return query.format(cols, table_alias)
+
+def join_tables_no_select(base_query, query_args, columns, join_func):
+    query = base_query
+
+    select_str, from_join_str = join_func(query_args, columns)
+
+    query = base_query.format(select_str, from_join_str)
+
+    print(f"Got a query: {query}")
+
+    return query
