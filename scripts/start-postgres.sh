cc74906c42b2a342041c52473732e13c2611fcda
Pat McNeely <pmcneely@gwu.edu>
2025-12-19 12:30:51 -0500

    Add localhost port in dev mode

--- scripts/start-postgres.sh
+++ scripts/start-postgres.sh
@@ -5,7 +5,7 @@ MODE="${MODE:-"prod"}"
 if [ $MODE == "dev" ]; then
     docker run -d -e POSTGRES_PASSWORD=fhir -e POSTGRES_USER=postgres \
         -e POSTGRES_DB=fhir -v $PWD/fhir-data:/var/lib/postgres/data \
-        --network fhir-backplane --restart always \
+        --network fhir-backplane -p 5432:5432 --restart always \
         --name db-fhir postgres:18.0-alpine3.22
 elif [ $MODE == "prod" ]; then
     echo Running FHIR server in PRODUCTION mode
