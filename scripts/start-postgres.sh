8458d191fbd111b2f5dfb27c1b4195ea40bd0760
Pat McNeely <pmcneely@gwu.edu>
2025-12-19 12:28:12 -0500

    Grant a dev/prod mode to postgres. Default to prod, and direct DB persistance to a more amenable location.

--- scripts/start-postgres.sh
+++ scripts/start-postgres.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-docker run -d -e POSTGRES_PASSWORD=fhir -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=fhir -v $PWD/fhir-data:/var/lib/postgres/data \
-    --network fhir-backplane --name db-fhir postgres:18.0-alpine3.22
+MODE="${MODE:-"prod"}"
+
+if [ $MODE == "dev" ]; then
+    docker run -d -e POSTGRES_PASSWORD=fhir -e POSTGRES_USER=postgres \
+        -e POSTGRES_DB=fhir -v $PWD/fhir-data:/var/lib/postgres/data \
+        --network fhir-backplane --restart always \
+        --name db-fhir postgres:18.0-alpine3.22
+elif [ $MODE == "prod" ]; then
+    echo Running FHIR server in PRODUCTION mode
+    docker run -d -e POSTGRES_PASSWORD=fhir -e POSTGRES_USER=postgres \
+        -e POSTGRES_DB=fhir -v /data/arpah/imported/dockerdb:/var/lib/postgres/data \
+        --network fhir-backplane --restart always \
+        --name db-fhir postgres:18.0-alpine3.22
+fi
