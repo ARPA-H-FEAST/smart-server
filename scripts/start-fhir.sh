c339fd21b292702f7630bc318f4b9b4b47f4251e
Pat McNeely <pmcneely@gwu.edu>
2025-12-19 12:25:29 -0500

    Revert to production by default in FHIR start

--- scripts/start-fhir.sh
+++ scripts/start-fhir.sh
@@ -8,16 +8,17 @@
 # docker run -d -p 8080:8080 --network fhir-backplane \
 #     --name fhir-server --volume $DATA_DIR:/app/ feast-hapi-fhir-jpa-starter
 
-MODE="${MODE:-"dev"}"
+MODE="${MODE:-"prod"}"
 echo $MODE
 
 if [ $MODE == "dev" ]; then
     echo Running FHIR server in dev mode
     docker run -d -p 8080:8080 --network fhir-backplane \
-        --name fhir-server hapi-fhir-server:bitnami-tomcat-memDB
+        --name fhir-server hapi-fhir-server:bitnami-tomcat-postgres
 elif [ $MODE == "prod" ]; then
     echo Running FHIR server in PRODUCTION mode
     docker run -d -p 4243:8080 --network fhir-backplane --restart always \
-        --name fhir-server -v /data/arpah/dockerdb/:/data/arpah/db/ \
+        --name fhir-server \
 	    --log-opt max-size=10m hapi-fhir-server:bitnami-tomcat-h2fileDB
 fi
+# -v /data/arpah/dockerdb/:/data/arpah/db/
